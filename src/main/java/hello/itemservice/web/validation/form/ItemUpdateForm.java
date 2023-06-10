f4e155eadebbfc39f34b9b9f428b5baeb16db901
junnjuns <donamjun1@naver.com>
2023-06-11 02:25:55 +0900

    feat: Form 전송 객체 분리로 등록,수정 시 검증 분리

--- src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
+++ src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -1,4 +1,4 @@
-package hello.itemservice.domain.item;
+package hello.itemservice.web.validation.form;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
@@ -8,19 +8,18 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-public class Item {
+public class ItemUpdateForm {
 
+    @NotNull
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    //수정에서는 수량은 자유롭게 변경할 수 있다.
     private Integer quantity;
-
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
 }
